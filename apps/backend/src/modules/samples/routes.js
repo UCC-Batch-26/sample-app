1fb84085b3a95632187eb9a1e812c54fa1501d99
Jay Are Galinada <jayaregalinada@gmail.com>
2025-09-22 20:32:50 +0800

    feat: added route for deleting sample

--- apps/backend/src/modules/samples/routes.js
+++ apps/backend/src/modules/samples/routes.js
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import { addSample } from './controllers/add-sample.js';
 import { getAllSamples } from './controllers/get-all-samples.js';
 import { getSample } from './controllers/get-sample.js';
+import { deleteSample } from './controllers/delete-sample.js';
 
 const router = new Router();
 
 router.post('/', addSample);
 router.get('/', getAllSamples);
 router.get('/:id', getSample);
+router.delete('/:id', deleteSample);
 
 export default router;
